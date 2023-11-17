a8ea7280abeadf4c24b36140b5a60f6b2e79f092
Christophe.El-Zeinaty <christophe.el-zeinaty@insa-rennes.fr>
2023-11-17 17:17:58 +0100

    first commit

---
+++ add_two_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Add two numbers
+    int num1 = 5;
+    int num2 = 7;
+    int sum = num1 + num2;
+
+    // Print the result
+    printf("Sum of %d and %d is: %d\n", num1, num2, sum);
+
+    return 0;
+}
